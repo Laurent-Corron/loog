e1197c66cea9e285ed2654f1f8015a78bf63c1bf
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2019-12-22 12:49:12 +0100

    Temporary default CLI (convert to json)

--- src/loog/main.py
+++ src/loog/main.py
@@ -1,11 +1,17 @@
 # Copyright 2019 ACSONE SA/NV (<https://acsone.eu/>)
 
+import json
+import sys
+
 import click
+
+from .parser import enrich, parse_stream
 
 try:
     from importlib.metadata import version
 except ImportError:
     from importlib_metadata import version
+
 
 __version__ = version("loog")
 
@@ -16,7 +22,9 @@ An Odoo log parsing and enrichment library and CLI.
 Copyright 2019 ACSONE SA/NV (<https://acsone.eu/>)"""
 
 
-@click.group()
+@click.command()
 @click.version_option(version=__version__, message=__notice__)
 def main() -> None:
-    print()
+    for record in enrich(parse_stream(sys.stdin)):
+        json.dump(record, sys.stdout)
+        sys.stdout.write("\n")
